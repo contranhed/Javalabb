25dad4b83588139feba815b3c918a01188da0a68
Connie Tran Hedberg <connietranhedberg@gmail.com>
2022-01-16 11:48:57 +0100

    Add Person Class and list of persons

--- Salary/src/Main.java
+++ Salary/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Person> persons = listOfPersons();
+        System.out.println(persons);
+    }
 
+    private static List<Person> listOfPersons() {
+        return List.of(
+                new Person("Sebastian Persson", 30000, "male"),
+                new Person("Max Stolpe", 26000, "male"),
+                new Person("Connie Tran Hedberg", 38000, "female"),
+                new Person("Malin Eriksson", 33000, "female"),
+                new Person("Andreas Lundberg", 28000, "male"),
+                new Person("Pavel Drepalov", 29000, "male"),
+                new Person("Alexander Tenghamn", 36000, "male"),
+                new Person("Semir Pecar", 28000, "male"),
+                new Person("Suad Pecar", 28000, "male"),
+                new Person("Hassan Daabas", 47000, "male")
+        );
     }
 }
---
+++ Salary/src/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+    private String name;
+    private double salary;
+    private String gender;
+
+    public Person(String name, double salary, String gender) {
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
